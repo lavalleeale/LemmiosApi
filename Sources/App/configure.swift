4ef988af012d0cc7e48aeb9f94eecb6c3424d155
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-07-18 10:27:36 -0700

    Add apple-app-site-association

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,7 +8,7 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(.postgres(configuration: SQLPostgresConfiguration(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
