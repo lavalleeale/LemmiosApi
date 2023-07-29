c56003b62ba445f438500ca40f4825ff7b3640c2
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-07-29 14:35:11 -0700

    Stagger requests

--- Sources/App/Jobs/ReplyScheduler.swift
+++ Sources/App/Jobs/ReplyScheduler.swift
@@ -12,8 +12,8 @@ struct ReplySchedulerJob: AsyncScheduledJob {
             .set(\.$lastChecked, to: .now)
             .update()
         context.application.logger.info("Checking replies for \(users.count) users")
-        for user in users {
-            try await context.queue.dispatch(RepliesJob.self, user)
+        for user in users.enumerated() {
+            try await context.queue.dispatch(RepliesJob.self, user.element, delayUntil: Date.now + TimeInterval((user.offset % 3) * 15))
         }
     }
 }
