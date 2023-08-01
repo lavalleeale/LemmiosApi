404a8b3e4117954da9975258872bdfad39e19cdf
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-07-31 21:14:04 -0700

    Add timeout to reply checking

--- Sources/App/Jobs/Replies.swift
+++ Sources/App/Jobs/Replies.swift
@@ -9,65 +9,75 @@ struct RepliesJob: AsyncJob {
     typealias Payload = User
 
     func dequeue(_ context: Queues.QueueContext, _ payload: User) async throws {
-        context.application.logger.info("Getting replies for \(payload.username)")
-        var cancellable = Set<AnyCancellable>()
-        let lemmyApi = try! LemmyApi(baseUrl: payload.instance)
-        lemmyApi.setJwt(jwt: payload.id!)
-        let (countResponse, error) = await withCheckedContinuation { continuation in
-            lemmyApi.getUnreadCount { unread, error in
-                continuation.resume(returning: (unread, error))
-            }.store(in: &cancellable)
-        }
-        if let error = error {
-            if case .lemmyError(let message, code: _) = error {
-                context.application.logger.error("Failed to get replies for \(payload.username) with error \(message)")
-//                            try await user.delete(on: context.application.db)
-            } else {
-                context.application.logger.error("Failed to get replies for \(payload.username) with unknown error \(error)")
+        let getRepliesTask = Task {
+            context.application.logger.info("Getting replies for \(payload.username)")
+            var cancellable = Set<AnyCancellable>()
+            let lemmyApi = try! LemmyApi(baseUrl: payload.instance)
+            lemmyApi.setJwt(jwt: payload.id!)
+            let (countResponse, error) = await withCheckedContinuation { continuation in
+                lemmyApi.getUnreadCount { unread, error in
+                    continuation.resume(returning: (unread, error))
+                }.store(in: &cancellable)
             }
-        } else if let countResponse = countResponse {
-            let total = countResponse.replies + countResponse.private_messages
-            if total != 0 {
-                _ = context.application.apns.send(
-                    APNSwiftPayload(badge: total),
-                    to: payload.deviceToken
-                )
-            }
-            if countResponse.replies != 0 {
-                let (repliesResponse, error) = await withCheckedContinuation { continuation in
-                    lemmyApi.getReplies(page: 1, sort: LemmyApi.Sort.New, unread: true) { replies, error in
-                        continuation.resume(returning: (replies, error))
-                    }.store(in: &cancellable)
+            if let error = error {
+                if case .lemmyError(let message, code: _) = error {
+                    context.application.logger.error("Failed to get replies for \(payload.username) with error \(message)")
+//                    try await user.delete(on: context.application.db)
+                } else {
+                    context.application.logger.error("Failed to get replies for \(payload.username) with unknown error \(error)")
                 }
-                guard let repliesResponse = repliesResponse else {
-                    return
-                }
-                let replies = repliesResponse.replies.filter { $0.counts.published > payload.lastChecked }
-                for reply in replies {
+            } else if let countResponse = countResponse {
+                let total = countResponse.replies + countResponse.private_messages
+                if total != 0 {
                     _ = context.application.apns.send(
-                        APNSwiftPayload(alert: .init(title: "New reply from \(reply.creator.name)", subtitle: reply.comment.content)),
+                        APNSwiftPayload(badge: total),
                         to: payload.deviceToken
                     )
                 }
-            }
-            if countResponse.private_messages != 0 {
-                let (messagesResponse, error) = await withCheckedContinuation { continuation in
-                    lemmyApi.getMessages(page: 1, sort: LemmyApi.Sort.New, unread: true) { messages, error in
-                        continuation.resume(returning: (messages, error))
-                    }.store(in: &cancellable)
+                if countResponse.replies != 0 {
+                    let (repliesResponse, error) = await withCheckedContinuation { continuation in
+                        lemmyApi.getReplies(page: 1, sort: LemmyApi.Sort.New, unread: true) { replies, error in
+                            continuation.resume(returning: (replies, error))
+                        }.store(in: &cancellable)
+                    }
+                    guard let repliesResponse = repliesResponse else {
+                        return
+                    }
+                    let replies = repliesResponse.replies.filter { $0.counts.published > payload.lastChecked }
+                    for reply in replies {
+                        _ = context.application.apns.send(
+                            APNSwiftPayload(alert: .init(title: "New reply from \(reply.creator.name)", subtitle: reply.comment.content)),
+                            to: payload.deviceToken
+                        )
+                    }
                 }
-                guard let messagesResponse = messagesResponse else {
-                    return
-                }
-                let messages = messagesResponse.private_messages.filter { $0.private_message.published > payload.lastChecked }
-                for message in messages {
-                    _ = context.application.apns.send(
-                        APNSwiftPayload(alert: .init(title: "New message from \(message.creator.name)", subtitle: message.private_message.content)),
-                        to: payload.deviceToken,
-                        loggerConfig: .none
-                    )
+                if countResponse.private_messages != 0 {
+                    let (messagesResponse, error) = await withCheckedContinuation { continuation in
+                        lemmyApi.getMessages(page: 1, sort: LemmyApi.Sort.New, unread: true) { messages, error in
+                            continuation.resume(returning: (messages, error))
+                        }.store(in: &cancellable)
+                    }
+                    guard let messagesResponse = messagesResponse else {
+                        return
+                    }
+                    let messages = messagesResponse.private_messages.filter { $0.private_message.published > payload.lastChecked }
+                    for message in messages {
+                        _ = context.application.apns.send(
+                            APNSwiftPayload(alert: .init(title: "New message from \(message.creator.name)", subtitle: message.private_message.content)),
+                            to: payload.deviceToken,
+                            loggerConfig: .none
+                        )
+                    }
                 }
             }
         }
+
+        let timeoutTask = Task {
+            try await Task.sleep(nanoseconds: 2_000_000_000)
+            getRepliesTask.cancel()
+        }
+
+        await getRepliesTask.value
+        timeoutTask.cancel()
     }
 }
